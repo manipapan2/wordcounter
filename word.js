44ae99c54ff53b96e08addbb04c5a06d13158d28
manipapan2 <105167359+manipapan2@users.noreply.github.com>
2022-09-12 20:52:54 +0430

    Letter Counter Added

--- word.js
+++ word.js
@@ -1,19 +1,10 @@
 document.getElementById("fars").addEventListener("click" , diri);
 document.getElementById("english").addEventListener("click" , diris);
-
-
+document.getElementById("contex").addEventListener("keyupw" , diris);
+document.getElementById("contex").addEventListener("input" , letlive)
 const textArea = document.getElementById('contex');
-textArea.addEventListener('input', () => {
-    var textLn =  textArea.value;
-    document.getElementById('wordspan').innerHTML=getWordCount(textLn);
-});
+textArea.addEventListener('input', wordlive);
 
-
-const textAreaa = document.getElementById('contex');
-textAreaa.addEventListener('input', () => {
-    var textLn =  textAreaa.value;
-    document.getElementById('wordspan').innerHTML=getWordCount(textLn);
-});
 
 function diri(){
     document.getElementById("contex").style.direction="rtl";
@@ -26,7 +17,34 @@ function diris(){
 
 
 
+function wordlive(){
+    var textLn =  textArea.value;
+    var con = document.getElementById("contex")
+    if(textLn == "" || textLn == null){
+        document.getElementById("wordspan").innerHTML="0";
+    }
+    else{
+        document.getElementById('wordspan').innerHTML=getWordCount(textLn);
+    }
+}
+
+
 function getWordCount(str) {
     return str.trim().split(/\s+/).length; 
 }
 // can be split (" ")
+
+
+
+function letlive(){
+    var shomar = document.getElementById("contex").value.replace(/[^a-z]/gi, "").length;
+    document.getElementById("letspan").innerHTML=shomar;
+}
+
+
+function khali(){
+    var a  = document.getElementById("contex").value
+    if(a == "salam"){
+        document.getElementById("wordspan").innerHTML="3";
+    }
+}
